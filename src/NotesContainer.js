c549d25969069f98c1f5edcb2281111818a1fa5a
Erica Belzer <belzer49@gmail.com>
2023-06-27 10:42:37 -0500

    started fetch

--- src/NotesContainer.js
+++ src/NotesContainer.js
@@ -1,6 +1,15 @@
 // import SingleNote from './SingleNote'
+
+import { useState, useEffect } from "react"
+
  
 function NotesContainer() {
+const [notes, setNotes] = useState('')
+useEffect(() => {
+  fetch('http://localhost:4000/notes')
+  .then((r) => r.json())
+  .then((notes) => console.log(notes))
+})
   return (
     <div>
       { /* all the notes go here! */ }
